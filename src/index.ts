91b4860871be73826d514ec45f96a6303967a055
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-08 12:24:27 +0300

    refactored db

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,60 @@
 import { GraphQLServer } from "graphql-yoga";
 import { v1 } from "uuid";
-
-let users = [
-  {
-    id: "123098",
-    name: "kevin",
-    email: "kevin@gmail.com",
-    age: 21
-  },
-  {
-    id: "1298",
-    name: "wezo",
-    email: "wezo@gmail.com",
-    age: 26
-  },
-  {
-    id: "128",
-    name: "brian",
-    email: "brian@gmail.com",
-    age: 22
-  }
-];
-let posts = [
-  {
-    id: "jkhkj132",
-    title: "First blog",
-    body: "This is the first blog",
-    published: true,
-    author: "123098"
-  },
-  {
-    id: "jkj132",
-    title: "Second blog",
-    body: "This is the second blog",
-    published: false,
-    author: "1298"
-  },
-  {
-    id: "132",
-    title: "last blog",
-    body: "This is the last blog",
-    published: true,
-    author: "123098"
-  }
-];
-let comments = [
-  {
-    id: "jkhkj13",
-    text: "this is the First comment",
-    author: "1298",
-    post: "jkhkj132"
-  },
-  {
-    id: "jkj13",
-    text: "this is the Second comment",
-    author: "1298",
-    post: "jkj132"
-  },
-  {
-    id: "123",
-    text: "this is the third comment",
-    author: "128",
-    post: "jkhkj132"
-  },
-  {
-    id: "13",
-    text: "this is the last comment",
-    author: "123098",
-    post: "132"
-  }
-];
+import { comments, posts, users } from "./db";
 
 // RESOLVERS
 const resolvers = {
   Query: {
-    users(parent: any, args: { query?: string }, ctx: any, info: any) {
+    users(
+      parent: any,
+      args: { query?: string },
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
       if (!args.query) {
         return users;
       }
-      return users.filter(usr =>
+      return ctx.users.filter(usr =>
         usr.name.toLowerCase().includes(args.query!.toLowerCase())
       );
     },
-    posts(parent: any, args: { query?: string }, ctx: any, info: any) {
+    posts(
+      parent: any,
+      args: { query?: string },
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
       if (!args.query) {
         return posts;
       }
-      return posts.filter(
+      return ctx.posts.filter(
         pst =>
           pst.title.toLowerCase().includes(args.query!.toLowerCase()) ||
           pst.body.toLowerCase().includes(args.query!.toLowerCase())
       );
     },
-    comments(parent: any, args: { query?: string }, ctx: any, info: any) {
+    comments(
+      parent: any,
+      args: { query?: string },
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
       if (!args.query) {
         return comments;
       }
-      return comments.filter(
+      return ctx.comments.filter(
         cmt =>
           cmt.text.toLowerCase().includes(args.query!.toLowerCase()) ||
           cmt.author.toLowerCase().includes(args.query!.toLowerCase())
@@ -123,10 +81,14 @@ const resolvers = {
     createUser(
       parent: any,
       args: { data: { name: string; email: string; age: number } },
-      ctx: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
       info: any
     ) {
-      const emailExists = users.some(
+      const emailExists = ctx.users.some(
         usr => usr.email.toLowerCase() === args.data.email.toLowerCase()
       );
       if (emailExists) {
@@ -138,7 +100,7 @@ const resolvers = {
         email: args.data.email,
         age: args.data.age
       };
-      users.push(user);
+      ctx.users.push(user);
       return user;
     },
     createPost(
@@ -151,29 +113,37 @@ const resolvers = {
           author: string;
         };
       },
-      ctx: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
       info: any
     ) {
-      const authorExists = users.some(
+      const authorExists = ctx.users.some(
         usr => usr.id.toLowerCase() === args.data.author.toLowerCase()
       );
       if (!authorExists) {
         throw new Error("No author with that ID");
       }
       const post: typeof posts[0] = { id: v1(), ...args.data };
-      posts.push(post);
+      ctx.posts.push(post);
       return post;
     },
     createComment(
       parent: any,
       args: { data: { text: string; author: string; post: string } },
-      ctx: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
       info: any
     ) {
-      const postExists = posts.some(
+      const postExists = ctx.posts.some(
         pst => pst.id === args.data.post && pst.published
       );
-      const authorExists = users.some(usr => usr.id === args.data.author);
+      const authorExists = ctx.users.some(usr => usr.id === args.data.author);
       if (!postExists || !authorExists) {
         throw new Error("post or author doesnot exist");
       }
@@ -181,71 +151,157 @@ const resolvers = {
         id: v1(),
         ...args.data
       };
-      comments.push(comment);
+      ctx.comments.push(comment);
       return comment;
     },
-    deleteUser(parent: any, args: { id: string }, ctx: any, info: any) {
-      const userIndx = users.findIndex(usr => usr.id === args.id);
+    deleteUser(
+      parent: any,
+      args: { id: string },
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      const userIndx = ctx.users.findIndex(usr => usr.id === args.id);
       if (userIndx === -1) {
         throw new Error("No user with that id");
       }
-      const deletedUser = users.splice(userIndx, 1);
-      posts = posts.filter(pst => {
+      const deletedUser = ctx.users.splice(userIndx, 1);
+      ctx.posts = ctx.posts.filter(pst => {
         if (pst.author === args.id) {
-          comments = comments.filter(cmt => cmt.author !== args.id);
+          ctx.comments = ctx.comments.filter(cmt => cmt.author !== args.id);
         }
         return pst.author !== args.id;
       });
-      comments = comments.filter(cmt => cmt.author !== args.id);
+      ctx.comments = ctx.comments.filter(cmt => cmt.author !== args.id);
       return deletedUser[0];
     },
-    deletePost(parent: any, args: { id: string }, ctx: any, info: any) {
-      const postIndx = posts.findIndex(pst => pst.id === args.id);
+    deletePost(
+      parent: any,
+      args: { id: string },
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      const postIndx = ctx.posts.findIndex(pst => pst.id === args.id);
       if (postIndx === -1) {
         throw new Error("No post with that ID");
       }
-      const deletedPost = posts.splice(postIndx, 1);
-      comments = comments.filter(cmt => cmt.post !== args.id);
+      const deletedPost = ctx.posts.splice(postIndx, 1);
+      ctx.comments = ctx.comments.filter(cmt => cmt.post !== args.id);
       return deletedPost[0];
     },
-    deleteComment(parent: any, args: { id: string }, ctx: any, info: any) {
-      const commentIndx = comments.findIndex(cmt => cmt.id === args.id);
+    deleteComment(
+      parent: any,
+      args: { id: string },
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      const commentIndx = ctx.comments.findIndex(cmt => cmt.id === args.id);
       if (commentIndx === -1) {
         throw new Error("No comment with that id");
       }
-      const deletedComment = comments.splice(commentIndx, 1);
+      const deletedComment = ctx.comments.splice(commentIndx, 1);
       return deletedComment[0];
     }
   },
   Post: {
-    author(parent: typeof posts[0], args: any, ctx: any, info: any) {
-      return users.find(usr => usr.id === parent.author);
+    author(
+      parent: typeof posts[0],
+      args: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      return ctx.users.find(usr => usr.id === parent.author);
     },
-    comments(parent: typeof posts[0], args: any, ctx: any, info: any) {
-      return comments.filter(cmt => cmt.post === parent.id);
+    comments(
+      parent: typeof posts[0],
+      args: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      return ctx.comments.filter(cmt => cmt.post === parent.id);
     }
   },
   User: {
-    posts(parent: typeof users[0], args: any, ctx: any, info: any) {
-      return posts.filter(pst => pst.author === parent.id);
+    posts(
+      parent: typeof users[0],
+      args: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      return ctx.posts.filter(pst => pst.author === parent.id);
     },
-    comments(parent: typeof users[0], args: any, ctx: any, info: any) {
-      return comments.filter(cmt => cmt.author === parent.id);
+    comments(
+      parent: typeof users[0],
+      args: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      return ctx.comments.filter(cmt => cmt.author === parent.id);
     }
   },
   Comment: {
-    author(parent: typeof comments[0], args: any, ctx: any, info: any) {
-      return users.find(usr => usr.id === parent.author);
+    author(
+      parent: typeof comments[0],
+      args: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      return ctx.users.find(usr => usr.id === parent.author);
     },
-    post(parent: typeof comments[0], args: any, ctx: any, info: any) {
-      return posts.find(pst => pst.id === parent.post);
+    post(
+      parent: typeof comments[0],
+      args: any,
+      ctx: {
+        comments: typeof comments;
+        posts: typeof posts;
+        users: typeof users;
+      },
+      info: any
+    ) {
+      return ctx.posts.find(pst => pst.id === parent.post);
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: {
+    comments,
+    posts,
+    users
+  }
 });
 
 server.start((): void => {
